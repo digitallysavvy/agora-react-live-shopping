b608321956f39fe682eae90ceec4352cf8dd0bd9
Hermes Frangoudis <hermes.frangoudis@hermess-mbp-3.mynetworksettings.com>
2024-02-27 13:09:05 -0500

    refactored with new header

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { logout } from './firebase-config'
 import Header from './Header'
-import agoraLogo from './assets/agora-logo.svg'
 import './App.css'
 
 // Temp Dashboard placeholder using the default vite react page
@@ -10,19 +8,11 @@ function Dashboard () {
   return (
     <>
       <Header />
-      <div>
-        <a href="https://agora.io" target="_blank">
-          <img src={agoraLogo} className="logo" alt="Agora logo" />
-        </a>
-      </div>
-      <h1>Live Shopping</h1>
+      <h1>Live Shopping Sessions</h1>
       <div className="card">
-        <button onClick={() => logout() }>
-          Sign out
-        </button>
+        You don't have any sessions scheduled, let's change that.
       </div>
       <p className="read-the-docs">
-        You don't have any sessions scheduled, let's change that. <br/>
         Use  the button above to schedule your first session.
       </p>
     </>
