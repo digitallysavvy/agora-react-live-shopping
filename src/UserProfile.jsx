d35ef0ded6315f436d46ce15a173c4588b7eb34e
Hermes Frangoudis <hermes.frangoudis@hermess-mbp-3.mynetworksettings.com>
2024-02-27 18:08:39 -0500

    updated toggle logic

--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -1,11 +1,12 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useAuth } from './AuthProvider'
 import { logout } from './firebase-config'
 
 const UserProfile = () => {
   const [showOptions, setShowOptions] = useState(false);
   const { currentUser } = useAuth();
+  const accountRef = useRef(null)
 
   const handleLogout = async () => {
     try {
@@ -16,8 +17,25 @@ const UserProfile = () => {
     }
   };
 
+  const toggleOptions = () => {
+    setShowOptions(previousShowOptions => !previousShowOptions)
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if(accountRef.current && !accountRef.current.contains(event.target)){
+        setShowOptions(false) // Hide the options if users click outside this component
+      }
+    }
+    // add mouse click listener 
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [accountRef])
+
   return (
-    <div id="account-container">
+    <div id="account-container" ref={accountRef}>
       <div id="profile-button" onClick={() => setShowOptions(!showOptions)}>
         {currentUser?.email.split("@")[0]}
       </div>
