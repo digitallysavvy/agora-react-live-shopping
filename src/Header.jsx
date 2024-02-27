d346e9f46c63e24605756d75c2d08244a780524c
Hermes Frangoudis <hermes.frangoudis@hermess-mbp-3.mynetworksettings.com>
2024-02-27 12:46:18 -0500

    added logo to header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,12 @@
 
 import React from 'react';
+import agoraLogo from './assets/agora-logo.svg'
 import UserProfile from './UserProfile';
 
 const Header = () => {
     return (
         <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px', background: '#f0f0f0' }}>
-            <img src="/path-to-your-logo.png" alt="Logo" style={{ width: '100px' }} />
+            <img src="{agoraLogo}" alt="Logo" style={{ width: '200px' }} />
             <nav>
                 <button>Create Session</button>
             </nav>
