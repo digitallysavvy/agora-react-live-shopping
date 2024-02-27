fa425797bdfc9e8bf5cc50113d3fe5e1c285b5e8
Hermes Frangoudis <hermes.frangoudis@hermess-mbp-3.mynetworksettings.com>
2024-02-27 18:01:32 -0500

    updated styles

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,15 +4,15 @@ import agoraLogo from './assets/agora-logo.svg'
 import UserProfile from './UserProfile';
 
 const Header = () => {
-    return (
-        <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px', background: '#f0f0f0' }}>
-            <img src={agoraLogo} className="logo" alt="Agora logo" style={{ width: '200px' }} />
-            <nav>
-                <button>Create Session</button>
-            </nav>
-            <UserProfile />
-        </header>
-    );
+  return (
+    <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px', background: '#f0f0f0' }}>
+      <img src={agoraLogo} className="logo" alt="Agora logo" style={{ width: '200px' }} />
+      <nav>
+        <button>Schedule Session</button>
+      </nav>
+      <UserProfile />
+    </header>
+  );
 };
 
 export default Header;
--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -4,31 +4,31 @@ import { useAuth } from './AuthProvider'
 import { logout } from './firebase-config'
 
 const UserProfile = () => {
-    const [showOptions, setShowOptions] = useState(false);
-    const { currentUser } = useAuth();
+  const [showOptions, setShowOptions] = useState(false);
+  const { currentUser } = useAuth();
 
-    const handleLogout = async () => {
-        try {
-            await logout();
-            console.log('User signed out');
-        } catch (error) {
-            console.error('Sign out error:', error);
-        }
-    };
+  const handleLogout = async () => {
+    try {
+      await logout();
+      console.log('User signed out');
+    } catch (error) {
+      console.error('Sign out error:', error);
+    }
+  };
 
-    return (
-        <div id="profile-container">
-            <div id="profile-button" onClick={() => setShowOptions(!showOptions)}>
-                {currentUser?.email.split("@")[0]}
-            </div>
-            {showOptions && (
-                <div id="account-button-container" >
-                    <button class="account-button" onClick={() => console.log('Edit Account Settings')}>Edit Account</button>
-                    <button class="account-button" onClick={handleLogout}>Logout</button>
-                </div>
-            )}
+  return (
+    <div id="account-container">
+      <div id="profile-button" onClick={() => setShowOptions(!showOptions)}>
+        {currentUser?.email.split("@")[0]}
+      </div>
+      {showOptions && (
+        <div id="account-dropdown" >
+          <div id="account-settings" class="account-button" onClick={() => console.log('Edit Account Settings')}>Edit Account</div>
+          <button class="account-button" onClick={handleLogout}>Logout</button>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default UserProfile;
