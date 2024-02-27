f2bdffc9e4268764c02296c4373b4fcfac5f5dc1
Hermes Frangoudis <hermes.frangoudis@hermess-mbp-3.mynetworksettings.com>
2024-02-26 19:51:25 -0500

    updated dashboard

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
+import React from 'react'
 import { logout } from './firebase-config'
-import reactLogo from './assets/react.svg'
 import agoraLogo from './assets/agora-logo.svg'
 import './App.css'
 
 // Temp Dashboard placeholder using the default vite react page
 function Dashboard () {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -14,16 +12,8 @@ function Dashboard () {
         <a href="https://agora.io" target="_blank">
           <img src={agoraLogo} className="logo" alt="Agora logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>Live Shopping</h1>
       <div className="card">
         <button onClick={() => logout() }>
           Sign out
