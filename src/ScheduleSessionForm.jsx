1bc5f2fd9ef69cf500c28c7cfc63ed405ef0b443
Hermes Frangoudis <hermes.frangoudis@hermess-mbp-3.mynetworksettings.com>
2024-02-28 19:31:36 -0500

    added form

--- src/ScheduleSessionForm.jsx
+++ src/ScheduleSessionForm.jsx
@@ -55,61 +55,61 @@ const ScheduleSessionForm = () => {
     const now = new Date();
     const hours = now.getHours().toString().padStart(2,'0')
     const min = now.getMinutes().toString().padStart(2,'0')
-    return `${hours}:${minutes}`
+    return `${hours}:${min}`
   }
 
-  return(<></>
-    // <form onSubmit={handleSubmit}>
-    //   <DatePicker selected={selectedDate} onChange={ date => setSelectedDate(date) } />
-    //   <TimePicker value={selectedTime} onChange={setSelectedTime} />
-    //   <TimezoneSelect value={selectedTimezone} onChange={setSelectedTimezone} />
-    //   <TextField 
-    //     type='number'
-    //     label='Number of Hosts'
-    //     value={numberOfHosts}
-    //     onChange={ e => setNumberOfHosts(e.target.value) }
-    //     inputProps={{ min: 1, max: 16 }}
-    //   />
-    //   {/* show the input for cohosts */}
-    //   {numberOfHosts > 1 && (
-    //     <FormControl fullWidth>
-    //       <InputLabel id="cohosts-label">Cohosts:</InputLabel>
-    //       <Select
-    //         labelId="cohosts-label"
-    //         id="cohosts-select"
-    //         multiple
-    //         value={selectedCohosts}
-    //         onChange={handleCohostSelect}
-    //         input={<OutlinedInput id="cohosts-select-multi-chip" label="Cohosts" />}
-    //         renderValue={(selected) => (
-    //           <div style={{ display: 'flex', flexWrap: 'wrap', gap: 0.5}}>
-    //             { selected.map(value => <Chip key={value} label={value}/>) }
-    //           </div>
-    //         )}
-    //       >
-    //         {users.map((user) => (
-    //           <MenuItem key={user.id} value={user.name}>
-    //             {user.name}
-    //           </MenuItem>
-    //         ))}
-    //       </Select>
-    //     </FormControl>
-    //   )}
-    //   {/* TODO: Add Product Selection */}
-    //   <FormControlLabel
-    //     control={ <Switch checked={isPrivate} onChange={() => setIsPrivate(!isPrivate)} /> }
-    //     label='Private Event'
-    //   />
-    //   {isPrivate && (
-    //     <TextField
-    //       label='Private Passcode'
-    //       type='password'
-    //       value={privatePasscode}
-    //       onChange={ e => setPrivatePasscode(e.target.value) }
-    //     />
-    //   )}
-    //   <button type='submit'>Schedule</button>
-    // </form>
+  return(
+    <form onSubmit={handleSubmit}>
+      <DatePicker selected={selectedDate} onChange={ date => setSelectedDate(date) } />
+      <TimePicker value={selectedTime} onChange={setSelectedTime} />
+      <TimezoneSelect value={selectedTimezone} onChange={setSelectedTimezone} />
+      <TextField 
+        type='number'
+        label='Number of Hosts'
+        value={numberOfHosts}
+        onChange={ e => setNumberOfHosts(e.target.value) }
+        inputProps={{ min: 1, max: 16 }}
+      />
+      {/* show the input for cohosts */}
+      {numberOfHosts > 1 && (
+        <FormControl fullWidth>
+          <InputLabel id="cohosts-label">Cohosts:</InputLabel>
+          <Select
+            labelId="cohosts-label"
+            id="cohosts-select"
+            multiple
+            value={selectedCohosts}
+            onChange={handleCohostSelect}
+            input={<OutlinedInput id="cohosts-select-multi-chip" label="Cohosts" />}
+            renderValue={(selected) => (
+              <div style={{ display: 'flex', flexWrap: 'wrap', gap: 0.5}}>
+                { selected.map(value => <Chip key={value} label={value}/>) }
+              </div>
+            )}
+          >
+            {users.map((user) => (
+              <MenuItem key={user.id} value={user.name}>
+                {user.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      )}
+      {/* TODO: Add Product Selection */}
+      <FormControlLabel
+        control={ <Switch checked={isPrivate} onChange={() => setIsPrivate(!isPrivate)} /> }
+        label='Private Event'
+      />
+      {isPrivate && (
+        <TextField
+          label='Private Passcode'
+          type='password'
+          value={privatePasscode}
+          onChange={ e => setPrivatePasscode(e.target.value) }
+        />
+      )}
+      <button type='submit'>Schedule</button>
+    </form>
   )
 }
 
