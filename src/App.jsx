287570ad8031e377157d7d4d1b49eaee97dafc8f
Hermes Frangoudis <hermes.frangoudis@hermess-mbp-3.mynetworksettings.com>
2024-02-29 12:16:49 -0500

    removed mui style provider

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,24 @@ import FirebaseAuthUI from './FirebaseAuthUI'
 import Dashboard from "./Dashboard"
 import ScheduleSession from "./ScheduleSession"
 
-import { ThemeProvider, CssBaseline } from "@mui/material"
 import './App.css'
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <CssBaseline />
-      <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            <Route exact path='/login' element={ <FirebaseAuthUI/> }/>
-            {/* Wrap private routes in layout route that checks auth status */}
-            <Route element={<PrivateLayout/>} >
-              <Route exact path='/dashboard' element={ <Dashboard/> }/>
-              <Route exact path='/schedule' element={ <ScheduleSession/> }/>
-            </Route>
-            {/* Redirect all un-known routes to dashboard */}
-            <Route path="*" element={ <Navigate to='/dashboard' replace/> }/>
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route exact path='/login' element={ <FirebaseAuthUI/> }/>
+          {/* Wrap private routes in layout route that checks auth status */}
+          <Route element={<PrivateLayout/>} >
+            <Route exact path='/dashboard' element={ <Dashboard/> }/>
+            <Route exact path='/schedule' element={ <ScheduleSession/> }/>
+          </Route>
+          {/* Redirect all un-known routes to dashboard */}
+          <Route path="*" element={ <Navigate to='/dashboard' replace/> }/>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   )
 }
 
